29b08eb19c856250d5d0b8140d8c9561c513329a
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-11-13 15:02:17 +0200

    Added some plotting tests.

---
+++ test/BresenhameTest/BresenhameTest/Tests/AsciiSceneTest.h
@@ -0,0 +1,32 @@
+#ifndef __ASCII_SCENE_TEST_H__
+#define __ASCII_SCENE_TEST_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class AsciiSceneTest : public CPPUNIT_NS::TestFixture
+{
+CPPUNIT_TEST_SUITE( AsciiSceneTest );
+    CPPUNIT_TEST( TestEmptyArrayProducesEmptyScene        );
+    CPPUNIT_TEST( TestSinglePointSegmentRenderedCorrectly );
+    CPPUNIT_TEST( TestHorizontalSegmentRenderedCorrectly  );
+    CPPUNIT_TEST( TestVerticalSegmentRenderedCorrectly    );
+    CPPUNIT_TEST( TestAverageSegmentRenderedCorrectly     );
+    CPPUNIT_TEST( TestMultipleSegmentsRenderedCorrectly   );
+CPPUNIT_TEST_SUITE_END();
+
+public:
+    void TestEmptyArrayProducesEmptyScene       ();
+    void TestSinglePointSegmentRenderedCorrectly();
+    void TestHorizontalSegmentRenderedCorrectly ();
+    void TestVerticalSegmentRenderedCorrectly   ();
+    void TestAverageSegmentRenderedCorrectly    ();
+    void TestMultipleSegmentsRenderedCorrectly  ();
+
+
+public:
+    AsciiSceneTest(void);
+    virtual ~AsciiSceneTest(void);
+};
+
+
+#endif //__ASCII_SCENE_TEST_H__
---
+++ test/BresenhameTest/BresenhameTest/stdafx.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <AsciiArtParsers/AsciiArtParsers.h>
+#include <AsciiArtCore/Types/AsciiArtTypes.h>
+
+
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TextTestProgressListener.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+
+#include <stdexcept>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <strstream>
+#include <stdio.h>
+#include <tchar.h>
+
